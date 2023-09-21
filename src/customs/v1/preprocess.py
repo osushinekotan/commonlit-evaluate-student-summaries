4b0661ae310c85704d29097ca75b48040616acb5
osushinekotan <spmt3710@gmail.com>
2023-09-21 21:17:16 +0900

    update :

--- src/customs/v1/preprocess.py
+++ src/customs/v1/preprocess.py
@@ -9,6 +9,11 @@ from src.utils.logger import Logger
 logger = Logger(__name__)
 
 
+def run_preprocess(prompts_df: pd.DataFrame, summaries_df: pd.DataFrame):
+    output_df = pd.merge(summaries_df, prompts_df, how="left", on="prompt_id")
+    return output_df
+
+
 @hydra.main(version_base=None, config_path="/workspace/configs/", config_name="config")
 def run(cfg: DictConfig) -> None:
     filepath = Path(cfg.paths.misc_dir) / "train.csv"
@@ -17,7 +22,7 @@ def run(cfg: DictConfig) -> None:
     if not filepath.is_file():
         prompts_train_df = pd.read_csv(Path(cfg.paths.input_dir) / "prompts_train.csv")
         summaries_train_df = pd.read_csv(Path(cfg.paths.input_dir) / "summaries_train.csv")
-        train_df = pd.merge(summaries_train_df, prompts_train_df, how="left", on="prompt_id")
+        train_df = run_preprocess(prompts_df=prompts_train_df, summaries_df=summaries_train_df)
         train_df.to_csv(filepath, index=None)
         logger.info(f"save : {filepath}")
     else:
