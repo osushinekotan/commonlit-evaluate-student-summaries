da2463b64ac6e21310e0f00a5a088465e312f27e
osushinekotan <spmt3710@gmail.com>
2023-09-24 20:05:59 +0900

    add : deploy

--- src/utils/kaggle_utils.py
+++ src/utils/kaggle_utils.py
@@ -1,11 +1,17 @@
+import json
+import os
+import re
 import subprocess
+import tempfile
 from pathlib import Path
 
 from kaggle import KaggleApi
+from omegaconf import DictConfig
+from transformers import AutoConfig, AutoTokenizer
 
-from src.utils.logger import Logger
+from src.utils.logger import HydraLogger
 
-logger = Logger(__name__)
+logger = HydraLogger(__name__)
 
 
 def download_kaggle_competition_dataset(
@@ -24,3 +30,55 @@ def download_kaggle_competition_dataset(
         subprocess.run(["unzip", "-q", zipfile_path, "-d", out_dir])
     else:
         logger.info("Dataset already exists.")
+
+
+class Deploy:
+    def __init__(
+        self,
+        cfg: DictConfig,
+        client: "KaggleApi",
+    ):
+        self.cfg = cfg
+        self.client = client
+
+    def push_output(self) -> None:
+        # model and predictions
+        dataset_name = re.sub(r"[/_=]", "-", self.cfg.experiment_name)
+        metadata = make_dataset_metadata(dataset_name=dataset_name)
+        with tempfile.TemporaryDirectory() as tempdir:
+            with open(Path(tempdir) / "dataset-metadata.json", "w") as f:
+                json.dump(metadata, f, indent=4)
+            self.client.dataset_create_new(
+                folder=self.cfg.paths.output_dir,
+                public=False,
+                quiet=False,
+            )
+
+    def push_huguingface_model(self) -> None:
+        # pretrained tokenizer and config
+        model_name = self.cfg.model_name
+        tokenizer = AutoTokenizer.from_pretrained(model_name)
+        config = AutoConfig.from_pretrained(model_name)
+
+        dataset_name = re.sub(r"[/_]", "-", model_name)
+        metadata = make_dataset_metadata(dataset_name=dataset_name)
+
+        with tempfile.TemporaryDirectory() as tempdir:
+            config.save_pretrained(tempdir)
+            tokenizer.save_pretrained(tempdir)
+            with open(Path(tempdir) / "dataset-metadata.json", "w") as f:
+                json.dump(metadata, f, indent=4)
+
+            self.client.dataset_create_new(
+                folder=tempdir,
+                public=True,
+                quiet=False,
+            )
+
+
+def make_dataset_metadata(dataset_name: str) -> dict:
+    dataset_metadata = {}
+    dataset_metadata["id"] = f'{os.environ["KAGGLE_USERNAME"]}/{dataset_name}'
+    dataset_metadata["licenses"] = [{"name": "CC0-1.0"}]
+    dataset_metadata["title"] = dataset_name
+    return dataset_metadata
